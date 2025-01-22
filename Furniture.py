9f887f8e9ef0d876e3610296515091ccec78bd66
sirenscodeX <malakiking47@gmail.com>
2025-01-22 14:36:31 -0500

    Add files via upload

---
+++ Furniture.py
@@ -0,0 +1,12 @@
+item_name = "Tv Stand"  
+retail_price = 325.0    
+wholesale_price = 200.0 
+profit = retail_price - wholesale_price 
+sale_price = 243.75 
+sale_profit = sale_price - wholesale_price  
+print("Item Name:", item_name)  
+print("Retail Price: $", retail_price)  
+print("Wholesale Price: $", wholesale_price)    
+print("Profit: $", wholesale_price) 
+print("Sale Price: $", sale_price)  
+print("Sale Profit: $", sale_profit)
