d2a3a19cc96f87c629e5b1d34b8e469dcc98c440
sirenscodeX <malakiking47@gmail.com>
2025-01-23 14:48:42 -0500

    Add files via upload

---
+++ State Tax og.py
@@ -0,0 +1,14 @@
+salary = 1250.0 
+num_dependents = 2  
+state_tax=salary * 6.5 
+federal_tax=salary * 28.0 
+dependent_deduction=num_dependents * 2.5
+total_withholding=state_tax + federal_tax + dependent_deduction   
+take_home_pay=salary - total_withholding  
+print("State Tax: $",round(state_tax,2))  
+print("Federal Tax: $",round(federal_tax,2))  
+print("Dependent Deducation: $",round(dependent_deduction,2))  
+print("Salary:$",round(salary,2))   
+print("Take-Home Pay: $",round(take_home_pay,2))    
+     
+  
