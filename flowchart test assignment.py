f95bff5cc2b98d48d46136d243147d1628de489d
sirenscodeX <malakiking47@gmail.com>
2025-02-04 20:44:50 -0500

    Add files via upload

---
+++ flowchart test assignment.py
@@ -0,0 +1,20 @@
+input = "testScore" + "classRank"
+if "testScore" == 90:
+  if "classRank" == 25:
+    print("Accept")
+  else:
+    print("Reject")
+else:
+  if "testScore" == 80:
+    if "classRank" == 50:
+      print("Accept")
+    else:
+      print("Reject")
+  else:
+    if "testScore" == 87:
+      if "classRank" == 60:
+        print("Accept")
+      else:
+        print("Reject")
+    else:
+      print("Accept")
